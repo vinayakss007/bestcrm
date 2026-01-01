3fdc6340a12ec41d498aa228362734b095db0901
Raj kumar <baoxing100@gmail.com>
2026-01-01 09:16:36 +0000

    continue building dont sttop untill all done  for both frontend and back

--- src/app/(app)/tasks/page.tsx
+++ src/app/(app)/tasks/page.tsx
@@ -194,5 +194,3 @@ export default async function TasksPage() {
       </div>
   )
 }
-
-    
--- src/components/delete-task-dialog.tsx
+++ src/components/delete-task-dialog.tsx
@@ -67,5 +67,3 @@ export function DeleteTaskDialog({ taskId }: DeleteTaskDialogProps) {
     </AlertDialog>
   )
 }
-
-    
